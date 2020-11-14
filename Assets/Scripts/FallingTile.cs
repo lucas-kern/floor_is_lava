7ef980691214e924b43edfbd28d4b5df1512c843
lucas-kern <lucasdkern@gmail.com>
2020-11-14 17:38:15 -0600

    add falling tile

---
+++ Assets/Scripts/FallingTile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingTile : MonoBehaviour
+{
+
+     public float fallSpeed = .01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+ 
+     void Update() {
+         		if (transform.position.y < -25) {
+			Destroy(gameObject);
+		}
+         transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+     }
+ 
+    //  void OnMouseDown() {
+ 
+    //      renderer.enabled = false;
+ 
+    //  }
+ 
+ }
