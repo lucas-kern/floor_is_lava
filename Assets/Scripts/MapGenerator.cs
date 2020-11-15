f47c2cc1cac4b2aed179dd84cb19f1cb16dedfd7
lucas-kern <lucasdkern@gmail.com>
2020-11-14 18:57:23 -0600

    Create map prefab

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -14,7 +14,6 @@ public class MapGenerator : MonoBehaviour
     {
         map = new GameObject[mapSize,mapSize];
         Debug.Log("in here");
-        // map[0,1] = Instantiate(myPrefab, new Vector3(2f, 0f, 0f), Quaternion.identity);
         for(int y = 0; y < mapSize; y++){
             for(int x = 0; x < mapSize; x++){
                 map[y,x] = (GameObject)Instantiate(myPrefab, new Vector3(2.1f*x, 0f, 2.1f*y), Quaternion.identity);
