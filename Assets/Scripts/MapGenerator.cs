e99e2581117b5c604f4f36d5bbc57168abc7b959
Lucas Kern <lucasdkern@gmail.com>
2020-11-14 19:00:52 -0600

    Create a map dynamically of tiles (#3)
    * create tile generation
    
    * Create map prefab
    
    * Add map to tile scene
    
    * Merge master

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    int mapSize = 20;
+    public GameObject myPrefab;
+    public GameObject[,] map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = new GameObject[mapSize,mapSize];
+        Debug.Log("in here");
+        for(int y = 0; y < mapSize; y++){
+            for(int x = 0; x < mapSize; x++){
+                map[y,x] = (GameObject)Instantiate(myPrefab, new Vector3(2.1f*x, 0f, 2.1f*y), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
